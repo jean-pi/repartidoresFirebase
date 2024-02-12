e4defb6c472ab6c5d9c83155db9a17e1ea3e62f3
Jean Pierre Veliz <jeanpierreveliz26@gmail.com>
2024-02-11 21:16:03 -0500

    avances en componente add, tarea en curso: crear componente de eliminar cuenta

---
+++ src/components/loginSignupRegisterSistem/cardUserLogeado.jsx
@@ -0,0 +1,47 @@
+
+//css
+import stylesText from "../../styles/texts.module.css";
+import uiStyles from "../../styles/uiStyles.module.css";
+import cardUsercss from "../../styles/styleComponets/cardUserLogeado.module.css"
+
+//mi firebase config
+import {auth} from "../../firebase/firebaseMyConfig"
+
+//router dom
+import { useNavigate } from "react-router-dom";
+import { publicRoutes } from "../../models/routes";
+
+
+function CardUserLogeado() {
+
+    let userDataLocal = JSON.parse(localStorage.getItem("user"));
+
+        
+    const navigate = useNavigate();
+
+    function signOut(){
+        auth.signOut()
+            .then(()=>{
+                console.log("signOut");
+                localStorage.clear();
+                navigate(publicRoutes.PRESENTATION_PUBLIC)
+            },err => {
+                console.log(err);
+            })
+    }
+
+
+
+  return (
+    <div className={cardUsercss.cardUserContainer}>
+        <img className={cardUsercss.img} src={userDataLocal.photoURL} alt="userImg" />
+        <div className={cardUsercss.emailNameContainer}>
+            <span className={cardUsercss.emailNameContainer_email}>{userDataLocal.email}</span>
+            <span className={cardUsercss.emailNameContainer_name}>{userDataLocal.diplayName}</span>
+        </div>
+        <button className={cardUsercss.buttonDeleteAcount}>Eliminar cuenta</button>
+        <button className={cardUsercss.buttonLogOut} onClick={signOut}>Log out</button>
+    </div>
+  )
+}
+export default CardUserLogeado
--- src/routes/MyApp.jsx
+++ src/routes/MyApp.jsx
@@ -2,7 +2,8 @@
 
 //components
 import RepartidorBox from "../components/repartidorBox";
-import LogoutButton from "../components/loginSignupRegisterSistem/logoutButton"
+import CardUserLogeado from "../components/loginSignupRegisterSistem/cardUserLogeado";
+
 
 //css
 import stylesMyApp from "../styles/stylesRoutes/MyApp.module.css";
@@ -13,6 +14,23 @@ export default function MyApp(){
     let user = auth.currentUser;
     return(
         <div className={stylesMyApp.contenedorApp}>
+
+
+            <CardUserLogeado/>
+
+            <p className={stylesMyApp.descriptionProyect}>
+                Build a list containing time slots, in 30-min intervals.
+                Let it start at 8 am and end at 8 pm. Or a box with a block
+                Let it start at 8, then 8:30, 9, 9, 30... like this until 8pm. The company
+                has availability of 8 motorcyclists every 30 min. when someone does
+                clicking on one of these little boxes should take a biker resource.
+                In other words, a counter that starts at 8 and then goes down to 7, in addition to marking the
+                box in green If the same user clicks on the same box, he must release
+                the resource, if it was in green, you must release the resource, that is, the
+                counter again goes from 7 to 8. If other users have taken all
+                motorcyclists, the box should appear red and should not let me
+                take that schedule.
+            </p>
 
             <div className={stylesMyApp.repartidorBoxContenedor}>
                 <RepartidorBox/>
@@ -29,21 +47,7 @@ export default function MyApp(){
                 <RepartidorBox/>
             </div>
             
-            <br /><LogoutButton/><br />
-
-            <p>
-                Build a list containing time slots, in 30-min intervals.
-                Let it start at 8 am and end at 8 pm. Or a box with a block
-                Let it start at 8, then 8:30, 9, 9, 30... like this until 8pm. The company
-                has availability of 8 motorcyclists every 30 min. when someone does
-                clicking on one of these little boxes should take a biker resource.
-                In other words, a counter that starts at 8 and then goes down to 7, in addition to marking the
-                box in green If the same user clicks on the same box, he must release
-                the resource, if it was in green, you must release the resource, that is, the
-                counter again goes from 7 to 8. If other users have taken all
-                motorcyclists, the box should appear red and should not let me
-                take that schedule.
-            </p>
+  
             
             {/* {`uid = ${user.uid}`} <br />
             {`displayName = ${user.displayName}`} <br />
@@ -53,6 +57,14 @@ export default function MyApp(){
             <img src={user.photoURL} alt="" /> */}
 
             
+            <footer className={stylesMyApp.footer}>
+                <a className={stylesMyApp.footer_a} href="">©ejerciciofirebase.github.io</a>
+                <a className={stylesMyApp.footer_a} target="_blank" href="https://twitter.com/sweetJean26">↖X</a>
+                <a className={stylesMyApp.footer_a} target="_blank" href="https://github.com/jean-pi">↖Github</a>
+                <a className={stylesMyApp.footer_a} target="_blank" href="https://www.instagram.com/jeanpierre_veliz/">↖Instagram</a>
+            </footer>
+            
+
         </div>
     );
 }
